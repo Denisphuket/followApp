f3dea50a32c616893ce6e7c903385a7ebf4edd6e
Andreev Denis <andreevdenis@MacBook-Pro-Andreev.local>
2019-07-15 10:24:11 +0700

    Updates

---
+++ precache-manifest.e94ac7b301902260227cfafa5a1fa1d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/followApp/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "0d9e403b15279894187d",
+    "url": "/followApp/static/js/runtime~main.1b6df8e0.js"
+  },
+  {
+    "revision": "06fd85862ce15ca747f1",
+    "url": "/followApp/static/js/main.b581b08e.chunk.js"
+  },
+  {
+    "revision": "bb4993433713c6543cd2",
+    "url": "/followApp/static/js/2.0a778a57.chunk.js"
+  },
+  {
+    "revision": "06fd85862ce15ca747f1",
+    "url": "/followApp/static/css/main.3e9748b4.chunk.css"
+  },
+  {
+    "revision": "bb4993433713c6543cd2",
+    "url": "/followApp/static/css/2.a22b3ddb.chunk.css"
+  },
+  {
+    "revision": "efb1d67f72b0b254414969b0513aa8d6",
+    "url": "/followApp/index.html"
+  }
+];
